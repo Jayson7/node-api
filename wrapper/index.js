6b9802baf6667b6ee7b1c5bb28f85276d57e318e
jayson7 <lexxiijoo70@gmail.com>
2021-02-10 15:15:18 -0800

    node api

---
+++ wrapper/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var path = require('path');
+
+const app = express();
+app.use('/', express.static(path.join(__dirname, 'static')));
+
+app.listen(9999, () => {
+    console.log(' server up at 9999')
+})
